6075c6cb7df59ea915f5ff7bf567b5b9311a5732
Erik Goranson <esgoranson@gmail.com>
2023-10-22 21:16:41 -0600

    added tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-//import './assets/main.css'
+import '@/index.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
 //global layouts
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
-import EmptyLayout from '@/layouts/EmptyLayout.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import EmptyLayout from '@/layouts/EmptyLayout.vue';
 
 const app = createApp(App)
 
@@ -15,4 +15,4 @@ app.use(router)
 app.component("DefaultLayout", DefaultLayout);
 app.component("EmptyLayout", EmptyLayout);
 
-app.mount('#app')
+app.mount('#app');
