c11a8ba632912d344009d959780e6387a5a96f7d
Erik Goranson <esgoranson@gmail.com>
2023-10-26 17:32:24 -0600

    css fixes

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -5,7 +5,7 @@ import TheFooter from '@/components/Footer.vue';
 </script>
 
 <template >
-  <div class="flex flex-col items-center bg-[#1b1d1e] text-neutral-200  font-mono ">
+  <div class="flex flex-col items-center">
     <div class="container-lg max-w-[1000px]  mx-5" >
       <NavBar/>
       <RouterView/>
